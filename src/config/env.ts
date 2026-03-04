42dc5ea41b28baf627365851ccd0566f4e9cb4c6
Corneille <corneilletwagirimana@gmail.com>
2026-03-04 11:37:41 +0200

    build

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 10000,
   MONGO_URI: process.env.MONGO_URI || "",
   JWT_SECRET: process.env.JWT_SECRET || "defaultsecret" 
 };
