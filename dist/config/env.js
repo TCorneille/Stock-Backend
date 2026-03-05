aadc6630d812bac7d0983a07922ed5dc30c69883
Corneille <corneilletwagirimana@gmail.com>
2026-03-05 08:49:54 +0200

    error fix

--- dist/config/env.js
+++ dist/config/env.js
@@ -7,7 +7,7 @@ exports.env = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.env = {
-    PORT: process.env.PORT || 4000,
+    PORT: process.env.PORT || 10000,
     MONGO_URI: process.env.MONGO_URI || "",
     JWT_SECRET: process.env.JWT_SECRET || "defaultsecret"
 };
